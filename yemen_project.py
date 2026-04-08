8050e01d6cb22947e85849fda0926ee71ab6c9f0
celestinebrown814-create <celestinebrown814@gmail.com>
2026-04-08 00:32:58 +0000

    Create yemen_project.py script with extracted code from YEMENPROJECT.ipynb

---
+++ yemen_project.py
@@ -0,0 +1,10 @@
+import xarray as xr
+import numpy as np
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+
+# Install cartopy package
+import subprocess
+import sys
+subprocess.check_call([sys.executable, "-m", "pip", "install", "cartopy"])
